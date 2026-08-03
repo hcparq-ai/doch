d84483917029b8320275173211b007eece328a1c
hcparq-ai <hcp.arq@gmail.com>
2026-08-03 15:31:56 +0000

    Implementa DOCH20 V2 móvil

---
+++ public/service-worker.js
@@ -0,0 +1,3 @@
+const CACHE='doch20-v2',ASSETS=['/','/index.html','/styles.css','/app.js','/manifest.webmanifest','/icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/index.html')))));
